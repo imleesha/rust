82cd792e04667dbec110dae18017ac3a23ebb99a
imleesha <elishakjobe@gmail.com>
2023-04-02 20:36:28 -0500

    variables

--- src/main.rs
+++ src/main.rs
@@ -8,5 +8,16 @@ use std::cmp::Ordering;
 
 
 fn main() {
-    println!("Hello, world!");
+    const ONE_MIL: u32 = 1_000_000;
+    const PI: f32 = 3.4192;
+    let age: &str = "37";
+    let mut age: u32 = age.trim().parse()
+    .expect( "Age wasn't assigned a number");
+    age = age + 1;
+    println!("I'm {} and I want ${}",age, ONE_MIL);
+
+
+
+
 }
+
